67c6685a64515eabf65ddfab8fff6ad89da1e184
swaich-Techno <swaichtecho@gmail.com>
2026-06-05 01:01:18 +0530

    Delete lib directory

--- lib/security/password.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export async function verifyPassword(password: string, passwordHash: string) {
-  return bcrypt.compare(password, passwordHash);
-}
-
-export async function hashPassword(password: string) {
-  return bcrypt.hash(password, 12);
-}
